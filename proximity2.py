5cd1b81beb28e7e7e64a984d8d7e098f7a0f6c50
jmh-1988 <joeyhidalgo88@gmail.com>
2026-04-06 18:13:40 -0700

    Update proximity2.py

--- proximity2.py
+++ proximity2.py
@@ -3,12 +3,12 @@ import time
 
 #GPIO pin setup
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT)
-GPIO.setup(15, GPIO.IN)
+GPIO.setup(17, GPIO.OUT)
+GPIO.setup(27, GPIO.IN)
 GPIO.setup(18, GPIO.OUT)
 
-GPIO.output(14, GPIO.LOW)
-time.sleep(0.1)
+GPIO.output(17, GPIO.LOW)
+time.sleep(0.05)
 
 def measure_distance(timeout=0.023):
    #added a timeout variable
@@ -16,24 +16,25 @@ def measure_distance(timeout=0.023):
    #34300 cm/s x 0.023s = 789cm
    #789cm/2 = 394cm measurable (3.94m)(sensor max range 4m)
    #Trigger ultrasonic pulse
-   GPIO.output(14, GPIO.HIGH)
+   GPIO.output(17, GPIO.HIGH)
    time.sleep(0.00001) #10 us pulse
-   GPIO.output(14, GPIO.LOW)
-   
+   GPIO.output(17, GPIO.LOW)
+
+   time.sleep(0.00002)
    #change time.time() to time.perf_counter()
    t0 = time.perf_counter()
 
    #Measure the time for echo
 
    #Invalid measurement (too far, poor sound reflection)
-   while GPIO.input(15) == 0:
+   while GPIO.input(27) == 0:
       if time.perf_counter() - t0 > timeout:
          return None 
    
    start_time = time.perf_counter()
 
    #Echo pulse duration too long(stuck high)
-   while GPIO.input(15) == 1:
+   while GPIO.input(27) == 1:
       if time.perf_counter() - start_time > timeout:
          return None 
       
@@ -52,12 +53,12 @@ try:
       #Turn on LED if distance is less than 10 cm
       if dist is None:
          GPIO.output(18, GPIO.LOW)
-      elif dist < 10:
+      elif dist < 20:
          GPIO.output(18, GPIO.HIGH)
       else:
          GPIO.output(18, GPIO.LOW)
         
-      time.sleep(1)  #delay before next reading
+      time.sleep(.2)  #delay before next reading
       
 except KeyboardInterrupt:
    print("Exiting Program")
